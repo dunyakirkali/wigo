936b8fcd4992ddf6f3952d23e771fba357f32497
Dunya Kirkali <dunyakirkali@gmail.com>
2014-09-27 03:20:28 +0200

    puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,1 +1,11 @@
+threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
+workers Integer(ENV['PUMA_WORKERS'] || 3)
 
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
